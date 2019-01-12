eb4361b748a34c2144700dde3b4323eada006bc7
Utashev Ruslan <tumanvoitel@gmail.com>
2019-01-12 16:17:16 +0700

    fix ui

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
+import AppBar from '@material-ui/core/AppBar/index';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
